7882bfce675b078a2666511ad9c25aca8f3f2111
mentejoao <skhulzxd@gmail.com>
2025-02-24 22:16:16 -0300

    fix: changing main to s3 env

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
+import boto3
+import os
 from fastapi import FastAPI
-from controllers.video_controller_hf import get_router
+from dotenv import load_dotenv
+from controllers.video_controller_s3 import get_router
 from transformers import AutoImageProcessor, Dinov2WithRegistersForImageClassification
-import torch
 
-# ref no momento: https://huggingface.co/WpythonW/dinoV2-deepfake-detector/tree/main
+load_dotenv()
 
-image_processor = AutoImageProcessor.from_pretrained('WpythonW/dinoV2-deepfake-detector')
+s3_client = boto3.client(
+    "s3",
+    region_name=os.getenv("AWS_S3_REGION"),
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+)
+
+image_processor = AutoImageProcessor.from_pretrained("WpythonW/dinoV2-deepfake-detector")
 model = Dinov2WithRegistersForImageClassification.from_pretrained("WpythonW/dinoV2-deepfake-detector")
 model.config.id2label = {0: "FAKE", 1: "REAL"}
-model.config.label2id = {"FAKE": 0, "REAL": 1}
+model.config.label2id = {"FAKE": 0, "REAL": 1"}
 
 app = FastAPI()
 
-app.include_router(get_router(image_processor, model))
+app.include_router(get_router(image_processor, model, s3_client))
 
-# test route
 @app.get("/")
 def home():
-    return {"message": "API de Upload de Vídeos está funfando!"}
+    return {"message": "API de Upload de Vídeos está funcionando!"}
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
+# ref no momento: https://huggingface.co/WpythonW/dinoV2-deepfake-detector/tree/main
